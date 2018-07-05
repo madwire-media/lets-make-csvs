29ac1e539b70713ecf5a402c9170053721ca908e
david <drj@rackfx.com>
2018-07-05 09:08:54 -0600

    example

--- testf.js
+++
@@ -1,6 +0,0 @@
-var replaceFirstLineOfFile = require("./lib/replaceFirstLineOfFile.js")
-
-replaceFirstLineOfFile("./test2.csv", "STUFF!!", function(err){
-  if(err) throw err
-  console.log('hixx')
-})
